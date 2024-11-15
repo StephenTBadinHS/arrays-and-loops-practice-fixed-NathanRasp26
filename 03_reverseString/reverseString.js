4cc72cc464ea332d483aeb8d35413dd56a7f5d30
Rasp <26RaspN@badinhs.org>
2024-11-14 22:04:08 -0500

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split('').reverse().join('')
 };
-
+console.log(reverseString('Hello There'));
 // Do not edit below this line
 console.log(reverseString);
